aaa6db63527deab996cc8779efbce3f9321c0e77
mrahm <mrahmed1287@gmail.com>
2023-02-11 19:45:53 +0500

    Initial commit

---
+++ lib/view/home_screen.dart
@@ -0,0 +1,83 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../model/postmodel.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Postmodel> PostList = [];
+  Future<List<Postmodel>> getPostApi() async {
+    final responce =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(responce.body.toString());
+    if (responce.statusCode == 200) {
+      for (Map i in data) {
+        PostList.add(Postmodel.fromJson(i));
+      }
+      return PostList;
+    } else {
+      return PostList;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Api Tutorial'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: FutureBuilder(
+                future: getPostApi(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                  } else {
+                    return ListView.builder(
+                        itemCount: PostList.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: EdgeInsets.all(0.6),
+                            child: Card(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Text(
+                                    'Title:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15),
+                                  ),
+                                  Text(PostList[index].title.toString()),
+                                  const Text(
+                                    'Description:',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 15),
+                                  ),
+                                  Text(PostList[index].body.toString())
+                                ],
+                              ),
+                            ),
+                          );
+                        });
+                  }
+                  return Container();
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
